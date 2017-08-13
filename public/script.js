3379a131d00fe825224a92ca02f8ffe75193b969
Robert Dickinson <bob.dickinson@gmail.com>
2017-08-13 01:05:24 -0700

    Work on action buttons

--- public/script.js
+++ public/script.js
@@ -1,16 +1,36 @@
 // 
-function createFolder () {
+function doCreateFolder () {
   bootbox.prompt("Create Folder", function (result) {
     if (result) {
-      var dbx = new Dropbox({ accessToken: token });
+      var dbx = new Dropbox({ accessToken: token })
       dbx.filesCreateFolder({ path: path + '/' + result })
       .then(function(response) {
-        console.log("Created folder:", response);
-        window.location.reload(true);
+        console.log("Created folder:", response)
+        window.location.reload(true)
       })
       .catch(function(error) {
         console.error(error)
-      });
+      })
     }
-  });
+  })
+}
+
+function doUploadFile () {
+  bootbox.alert("Upload File")
+}
+
+function doRename () {
+  bootbox.alert("Rename")
+}
+
+function doMove () {
+  bootbox.alert("Move")
+}
+
+function doCopy () {
+  bootbox.alert("Copy")
+}
+
+function doDelete () {
+  bootbox.alert("Delete")
 }
